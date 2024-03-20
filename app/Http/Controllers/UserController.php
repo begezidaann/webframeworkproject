5e9ba72cf00e8a440d075e4609f618f8ce7ccad3
begezidaann <zidanmuhamadherman@gmail.com>
2024-03-20 13:58:52 +0700

    second commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,64 +1,73 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Http\Request;
+use Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index(){
+        $users = User::All();
+        $ruangans = "Cek data";
+
+        return view('users/index', compact('users', 'ruangans'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function create(){
+        $roles = Role::All();
+        return view('users/create', compact('roles'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
-    {
-        //
+    {  
+        // dd($request);
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required',
+        ]);
+           
+        $data = $request->all();
+        // dd($data);
+        $check = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' =>$data['role']
+        ]);
+         
+        return redirect()->route('users.index')->withSuccess('Great! You have Successfully loggedin');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+    public function edit(User $user)
+    {   
+        $roles = Role::All();
+        return view('users.edit', compact('user', 'roles'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'role' => 'required',
+        ]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        if(!empty($request->password)) $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect()->route('users.index')->withSuccess('Great! You have Successfully loggedin');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect()->route('users.index')->with('success','user has been deleted successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HaiController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 
-Route::get('/postech/{nomor}/cek', [HaiController::class, 'index']);
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'dashboard']); 
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard'); 
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/users', [UserController::class, 'index'])->name('users');
+Route::resource('users', UserController::class); //users.index, users.create, users.edit, users.update, users.destroy
+Route::get('ruangans', [RuangController::class, 'indexPage'])->name('ruangans.page');
